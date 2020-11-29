133afc21d37ecff7d6d2b5d78add6b0cd6ea97cd
welearnedtohack <fmbcarreira@gmail.com>
2020-11-29 20:45:21 +0000

    restructed readfromfile function

--- src/CISUC/CISUC.java
+++ src/CISUC/CISUC.java
@@ -15,21 +15,32 @@ public class CISUC implements Serializable{
     }
     public static void main(String[] args) {
         CISUC cisuc = new CISUC();
-        cisuc.run();
+        cisuc.firstRun();
     }
 
-    public void run() {
-        readFromFile("pessoas.csv");
+    public void firstRun() {
+        readFromFile("efetive.csv", "publicacoes.csv", "pessoas.csv");
         System.out.println(investigators);
+        System.out.println(works);
     }
 
-    public void readFromFile(String file) {
-        String line = "";
+    public void readFromFile(String efetive, String publicacoes, String pessoas) {
+        String line;
         try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            while((line = br.readLine()) != null) {
+            BufferedReader br1 = new BufferedReader(new FileReader(efetive));
+            BufferedReader br2 = new BufferedReader(new FileReader(publicacoes));
+            BufferedReader br3 = new BufferedReader(new FileReader(pessoas));
+            while((line = br1.readLine()) != null) {
                 String[] split = line.split(",");
-                investigators.add(new Student(split[0],split[1],split[2],split[3],split[4],split[5]));
+                investigators.add(new EfetiveMember(split[0],split[1],split[2],split[3],Long.parseLong(split[4])));
+            }
+            while((line = br2.readLine()) != null) {
+                String[] split = line.split(",");
+                works.add(new Chapter(split[0],split[1],split[2],Integer.parseInt(split[3]),split[4],Integer.parseInt(split[5]),split[6],Integer.parseInt(split[7]),split[8],Integer.parseInt(split[9]),Integer.parseInt(split[10])));
+            }
+            while((line = br3.readLine()) != null) {
+                String[] split = line.split(",");
+                investigators.add(new Student(split[0], split[1], split[2], split[3], split[4], split[5]));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
